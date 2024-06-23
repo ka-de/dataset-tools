1f977e1250cbf55678b69fc792466fdea48ceca2
Balazs Horvath <acsipont@gmail.com>
2024-06-23 11:57:39 +0200

    omg

--- examples/nanorand_initialized_rng.rs
+++
@@ -1,8 +0,0 @@
-/// * Generating a number with an initialized RNG
-
-use nanorand::{ Rng, WyRand };
-
-fn main() {
-    let mut rng = WyRand::new();
-    println!("Random number: {}", rng.generate::<u64>());
-}
--- examples/nanorand_thread-local_rng.rs
+++
@@ -1,8 +0,0 @@
-/// * Generating a number with a thread-local RNG
-
-use nanorand::Rng;
-
-fn main() {
-    let mut rng = nanorand::tls_rng();
-    println!("Random number: {}", rng.generate::<u64>());
-}
