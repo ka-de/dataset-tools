74bea33933ad363fe0aedb50648c0f7c3700ffda
Balazs Horvath <acsipont@gmail.com>
2024-06-24 09:15:44 +0200

    awoo

--- compress-exe/src/main.rs
+++ compress-exe/src/main.rs
@@ -1,3 +1,16 @@
+/// This program compresses all executable files in a specified directory using UPX.
+///
+/// The program takes one optional command-line argument: the path to the directory containing the executables.
+/// If no argument is provided, it defaults to ".\\target\\x86_64-pc-windows-msvc\\release\\".
+///
+/// It iterates over each entry in the specified directory. If an entry is a file with a ".exe" extension,
+/// it attempts to compress it using the "upx" command with the "--best" option for maximum compression.
+///
+/// If the compression fails for any executable, it prints an error message to the standard error.
+///
+/// This program returns an `std::io::Result<()>`. If an I/O error occurs at any point (such as if the directory
+/// does not exist or is not readable), the program will return an `Err` variant containing the error.
+
 use std::env;
 use std::process::Command;
 use std::fs;
--- create-empty-caption-files/src/main.rs
+++ create-empty-caption-files/src/main.rs
@@ -1,22 +1,40 @@
 use std::fs::File;
-use walkdir::WalkDir;
+use std::io;
+use walkdir::{ DirEntry, WalkDir };
 
-fn create_caption_file(directory: &str) {
-    for entry in WalkDir::new(directory) {
-        let entry = entry.unwrap();
+fn is_not_git(entry: &DirEntry) -> bool {
+    entry.file_name().to_string_lossy() != ".git"
+}
+
+fn create_caption_file(directory: &str) -> io::Result<()> {
+    for entry in WalkDir::new(directory)
+        .into_iter()
+        .filter_entry(|e| is_not_git(e)) {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(err) => {
+                return Err(io::Error::new(io::ErrorKind::Other, err.to_string()));
+            }
+        };
         let path = entry.path();
-        let extension = path.extension().unwrap_or_default();
-        if ["jpg", "jpeg", "png"].contains(&extension.to_str().unwrap()) {
+        let extension = path
+            .extension()
+            .and_then(|s| s.to_str())
+            .unwrap_or_default();
+        if ["jpg", "jpeg", "png"].contains(&extension) {
             let caption_file = path.with_extension("txt");
             if !caption_file.exists() {
-                File::create(caption_file).unwrap();
+                File::create(caption_file)?;
             }
         }
     }
+    Ok(())
 }
 
 fn main() {
     let directory = "E:\\training_dir_staging";
-    create_caption_file(directory);
-    println!("Caption files created successfully.");
+    match create_caption_file(directory) {
+        Ok(_) => println!("Caption files created successfully."),
+        Err(err) => eprintln!("An error occurred: {}", err),
+    }
 }
